5c567f511496b26015ce5e614a1b8eecce9ea5f1
Bishal Sarker <mr.bishalsarker@gmail.com>
2020-11-25 22:17:41 +0600

    project initialized

---
+++ HYDB.API/Controllers/AuthController.cs
@@ -0,0 +1,81 @@
+﻿using HYDB.Services.DTO;
+using HYDB.Services.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HYDB.API.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserAccountInfo _userAccountInfoService;
+
+        public AuthController(IUserAccountInfo userAccountInfoService)
+        {
+            _userAccountInfoService = userAccountInfoService;
+        }
+
+        [HttpPost]
+        [Route("createaccount")]
+        public IActionResult CreateAccount(UserAccountPayload newUserAccountDetails)
+        {
+            return Ok(_userAccountInfoService.CreateAccount(newUserAccountDetails));
+        }
+
+        [Route("token/get")]
+        [HttpPost]
+        public IActionResult GenerateToken(UserAccountPayload newTokenRequest)
+        {
+            var authentication = _userAccountInfoService.AuthenticateUser(newTokenRequest);
+            if (authentication.IsSuccess)
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes("PDv7DrqznYL6nv7DrqzjnQYO9JxIsWdcjnQYL6nu0f");
+                var signinKey = new SymmetricSecurityKey(key);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Issuer = "hydb.xyz",
+                    Audience = "hydb.xyz",
+                    SigningCredentials = new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256),
+                    Subject = new ClaimsIdentity(new[]
+                    {
+                        new Claim(ClaimTypes.Email, newTokenRequest.UserName)
+                    }),
+
+                    Expires = DateTime.UtcNow.AddDays(15)
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                return Ok(new { Token = tokenHandler.WriteToken(token) });
+            }
+            else
+            {
+                return Unauthorized(authentication.Message);
+            }
+        }
+
+        [Route("token/verify")]
+        [HttpGet]
+        public IActionResult ValidateToken()
+        {
+            if ((HttpContext.User.Identity.IsAuthenticated))
+            {
+                return Ok(new { isValid = true });
+            }
+            else
+            {
+                return Ok(new { isValid = false });
+            }
+        }
+    }
+}
---
+++ HYDB.API/Startup.cs
@@ -0,0 +1,89 @@
+using HYDB.Services.Configurations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HYDB.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins, builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyHeader();
+                    builder.AllowAnyMethod();
+                });
+            });
+
+            services.AddControllers();
+
+            var key = Encoding.ASCII.GetBytes("PDv7DrqznYL6nv7DrqzjnQYO9JxIsWdcjnQYL6nu0f");
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "hydb.xyz",
+                    ValidAudience = "hydb.xyz",
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                };
+            });
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.InitializeHYDBServices();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
