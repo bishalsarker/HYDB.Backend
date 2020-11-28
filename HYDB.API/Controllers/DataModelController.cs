7400f3b4fc9537e4bd5130dc9ab9ab50d897494c
Bishal Sarker <mr.bishalsarker@gmail.com>
2020-11-29 02:10:50 +0600

    data model controller added

---
+++ HYDB.API/Controllers/DataModelController.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using HYDB.Services.DTO;
+using HYDB.Services.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HYDB.API.Controllers
+{
+    [Route("datamodel")]
+    [ApiController]
+    public class DataModelController : ControllerBase
+    {
+        private readonly DataModelManagement _dataModelService;
+
+        public DataModelController(IConfiguration config, IMapper mapper)
+        {
+            _dataModelService = new DataModelManagement(config, mapper);
+        }
+
+        [Route("addnew")]
+        [HttpPost]
+        [Authorize]
+        public IActionResult AddNewModel(DataModelPayload newDataModel)
+        {
+            return Ok(_dataModelService.AddNewDataModel(newDataModel, HttpContext.User.Claims.FirstOrDefault().Value));
+        }
+
+        [Route("get")]
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetDataModelListOrSingle()
+        {
+            if(Request.Query.ContainsKey("modelId"))
+            {
+                return Ok(_dataModelService.GetDataModel(Request.Query["modelId"]));
+            }
+            else
+            {
+                return Ok(_dataModelService.GetAllDataModel(HttpContext.User.Claims.FirstOrDefault().Value));
+            }
+        }
+
+        [Route("property/addnew")]
+        [HttpPost]
+        [Authorize]
+        public IActionResult AddNewProperty(DataModelPropertyPayload newDataModelProperty)
+        {
+            return Ok(_dataModelService.AddNewProperty(newDataModelProperty));
+        }
+
+        [Route("property/get")]
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetDataModelProperty(string propId)
+        {
+            if (Request.Query.ContainsKey("propId"))
+            {
+                return Ok(_dataModelService.GetProperty(Request.Query["propId"]));
+            }
+            else
+            {
+                return BadRequest("No property id is provided");
+            }
+        }
+
+        [Route("property/update")]
+        [HttpPost]
+        [Authorize]
+        public IActionResult UpdateProperty(DataModelPropertyPayload updateProperty)
+        {
+            return Ok(_dataModelService.EditProperty(updateProperty));
+        }
+
+        [Route("property/delete")]
+        [HttpGet]
+        [Authorize]
+        public IActionResult DeleteProperty()
+        {
+            if (Request.Query.ContainsKey("propId"))
+            {
+                return Ok(_dataModelService.DeleteProperty(Request.Query["propId"]));
+            }
+            else
+            {
+                return BadRequest("No property id is provided");
+            }
+        }
+    }
+}
